81642f91121e82398ceb0ed69929cd4d7268ee56
Ric Colasanti <rcolasanti@bournemouth.ac.uk>
2020-05-02 17:07:26 +0100

    Template project with ain css and main js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+async function fetchHtmlAsText(url) {
+    return await (await fetch(url)).text();
+}
+
+
+async  function getTemplate(elmt,page){
+      let itm = document.getElementById(elmt);
+      let address = "template/"+page+".html"
+      console.log(address);
+      itm.innerHTML = await fetchHtmlAsText(address)
+  }
+
+  getTemplate("itm","home")
